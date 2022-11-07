fe54c70d91de5c1c18e79aef3c629a2f144f466f
Afsar Uddin <afsar@globalsoftwarearchitects.net>
2022-11-07 21:27:21 +0600

    Creating ItemsProducts Page Controller

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurchaseManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurchaseManagementSystem.Controllers
+{
+    public class ItemController : Controller
+    {
+        public readonly ConnectionStringClass connectionStringClass;
+        public ItemController(ConnectionStringClass cs)
+        {
+            this.connectionStringClass = cs;
+        }
+        public IActionResult Display()
+        {
+            IList<Item> list = connectionStringClass.items.OrderByDescending(z => z.item_id).ToList();
+            return View(list);
+        }
+    }
+}
