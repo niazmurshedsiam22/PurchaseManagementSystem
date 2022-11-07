0a494ab76dafe2430b22b9c6db1a0fb0fe298daa
Afsar Uddin <afsar@globalsoftwarearchitects.net>
2022-11-07 17:16:15 +0600

    Connection String to SQL Server

---
+++ Models/ConnectionStringClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurchaseManagementSystem.Models
+{
+    public class ConnectionStringClass:DbContext
+    {
+        public ConnectionStringClass(DbContextOptions<ConnectionStringClass> options) : base(options)
+        {
+
+        }
+        public DbSet<Item> items { get; set; }
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<Vendor> vendors { get; set; }
+        public DbSet<Purchase> purchases { get; set; }
+        public DbSet<Issuance> issuances { get; set; }
+    }
+}
