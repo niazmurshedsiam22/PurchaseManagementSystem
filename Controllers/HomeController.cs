c0282c7953c8a8bacdadf80222dc6ae2fd91b291
Afsar Uddin <afsar@globalsoftwarearchitects.net>
2022-11-13 11:38:27 +0600

    Home Controller Design

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,14 @@ namespace PurchaseManagementSystem.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
